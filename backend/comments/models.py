da67c7693681fec0c31c5fda2d9738cc120b3c21
Cody <cemt5462@gmail.com>
2023-03-21 15:26:21 -0500

    Created comments and replies apps in backend + made models and updated recipe model with home_chef field.

---
+++ backend/comments/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from authentication.models import User
+from recipes.models import Recipe
+
+
+# Create your models here.
+
+class Comment(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+    
---
+++ backend/recipes/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from authentication.models import User
+
+# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    home_chef = models.CharField(max_length=200, default='Home Chef')
+    description = models.TextField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    image = models.ImageField(upload_to='recipes/', blank=True, null=True)
+    serving_size = models.PositiveIntegerField(default=1)
+
+
+
