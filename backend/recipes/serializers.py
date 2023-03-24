d6ecec00a52e857ba549fecf1852a21887e6ff04
Cody <cemt5462@gmail.com>
2023-03-24 15:07:30 -0500

    Completed function to get recipe ingredients by recipe id.

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Recipe
+from recipe_ingredient.models import RecipeIngredient
+
 
 class RecipeSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(required=False)
@@ -10,6 +12,18 @@ class RecipeSerializer(serializers.ModelSerializer):
         depth = 1
 
     user_id = serializers.IntegerField(write_only=True) #Allows us to use the user_id in the form-data for testing.
+
+
+#Added a new serializer that takes in a function to get the name of ingredient. This allows for 1 serializer to be used in the vew function. 
+class RecipeIngredientWithIngredientSerializer(serializers.ModelSerializer):
+    ingredient_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = RecipeIngredient
+        fields = ['id', 'ingredient_name', 'quantity', 'units']
+
+    def get_ingredient_name(self, obj):
+        return obj.ingredient_id.name
 
 
 
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -3,12 +3,13 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from recipes.models import Recipe
-from recipes.serializers import RecipeSerializer
+from recipes.serializers import RecipeSerializer, RecipeIngredientWithIngredientSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.parsers import MultiPartParser, FormParser
 from recipe_ingredient.models import RecipeIngredient
 from ingredients.models import Ingredient
 from ingredients.serializers import IngredientSerializer
+from recipe_ingredient.serializers import RecipeIngredientSerializer
 
 
 # Create your views here.
@@ -47,18 +48,42 @@ def get_recipe_by_id(request, pk):
         recipe.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_all_ingredients_of_recipe(request, pk):
+    #Filtering RecipeIngredient for Ingredient items that are attached to recipe_id
     recipe_ingredients = RecipeIngredient.objects.filter(recipe_id=pk)
-
-    print(recipe_ingredients[0].ingredient_id)
-    recipe_ingredients_list = []
-    for item in recipe_ingredients:
-        ingredient_item = {
-            
-        }
-        # ingredient = Ingredient.objects.get(id=item.ingredient_id)
-        recipe_ingredients_list.append(item.ingredient_id)
-    serializer = IngredientSerializer(recipe_ingredients_list, many=True)
+    #New serializer to serialize recipe_ingredient and get ingredient name. 
+    serializer = RecipeIngredientWithIngredientSerializer(recipe_ingredients, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+
+# @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+# def get_all_ingredients_of_recipe(request, pk):
+#     #Filtering RecipeIngredient for Ingredient items that are attached to recipe_id
+#     recipe_ingredients = RecipeIngredient.objects.filter(recipe_id=pk)
+#     print(recipe_ingredients[0].ingredient_id)
+#     #Emtpy array to hold list of recipe ingredients
+#     recipe_ingredients_list = []
+#     #Loop to iterate over each item from filter. Bring in qty and units from RecipeIngredient.
+#     for item in recipe_ingredients:
+#         #List of the data I want
+#         ingredient_item = {
+#             'name':item.ingredient_id.name,
+#             'quantity':item.quantity,
+#             'units':item.units
+#         }
+#         #Add each item to my recipe_ingredients_list
+#         recipe_ingredients_list.append(ingredient_item)
+#     serializer_measure = RecipeIngredientSerializer(recipe_ingredients_list, many=True)
+#     serializer_ingredient = IngredientSerializer(recipe_ingredients_list, many=True)
+#     #List of data coming in from 2 serializers
+#     data = {
+#         'measure': serializer_measure.data,
+#         'ingredient': serializer_ingredient.data
+#     }
+#     return Response(data, status=status.HTTP_200_OK)
